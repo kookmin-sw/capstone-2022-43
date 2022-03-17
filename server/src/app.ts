aec899cb1ea28e0fc305ad38098abd68c3b81e14
uyimjunho <joonno94@kookmin.ac.kr>
2022-03-17 23:12:53 +0900

    feat: log 저장 기능 winston을 사용하여 로그를 파일에 저장할 수 있게 함.

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,25 +3,36 @@ import HttpException from "./middlewares/HttpException";
 import 'dotenv/config';
 
 import indexRouter from './routes/index';
+import logger from './middlewares/logger';
+
 
 const app = express();
+const requestIp = require('request-ip');
+const Sniffr = require('sniffr');
 
 app.set('port', process.env.PORT || 8080);
 
 app.use('/', indexRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
+
     const error =  new HttpException(404, `Not exist ${ req.method } ${ req.url } router`);
     next(error);
 });
 
 app.use((err: HttpException, req: Request, res: Response, next: NextFunction) => {
-    console.log(err);
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV === 'develop' ? err : {};
+    const userAgent = req.headers['user-agent'];
+    const ip = requestIp.getClientIp(req);
+    const s = new Sniffr;
+    s.sniff(userAgent);
+    const userInfo : string = ` IP: ${ ip } OS : ${ s.os.name } Browser : ${ s.browser.name }`
+    logger.error(err.status + " " + err.message + userInfo);
+
     res.status(err.status || 500).send();
 });
 
-app.listen(app.get('port'), () => {
+
+
+app.listen(app.get('port'),'0.0.0.0' ,() => {
     console.log('Running server...');
 });
