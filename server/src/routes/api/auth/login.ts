f2bf063e07e0422af1a394bddf1800de7d7481dc
ASak1104 <khw56184@gmail.com>
2022-03-26 00:55:55 +0900

    feat: add token to response data
    milestone #13

--- server/src/routes/api/auth/login.ts
+++ server/src/routes/api/auth/login.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import passport from "passport";
+import jwt from "jsonwebtoken";
+import HttpException from "../../../@types/HttpException";
 
 
 const router = express.Router();
@@ -10,16 +12,21 @@ router.post('/', (req: Request, res: Response, next: NextFunction) => {
             return next(authError);
         }
         if (!user) {
-            return res.json({
-                message: info.message,
-            });
+            return next(new HttpException(400, info.message));
         }
 
-        // add token soon
+        const token = jwt.sign({
+            uuid: user.uuid,
+            email: user.email,
+            name: user.name
+        }, process.env.JWT_SECRET || '', {
+            expiresIn: '1h',
+            issuer: 'BAETAVERSE'
+        });
 
         return res.status(200).json({
             message: info.message,
-            token: null,
+            token,
         });
     }) (req, res, next);
 });
