e640c66aba172b283c92683cc8d561a365c5cf90
ASak1104 <khw56184@gmail.com>
2022-03-26 15:38:08 +0900

    test vercel extended

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,7 @@ const app: express.Application = express();
 
 app.set('port', process.env.PORT || 8080);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
