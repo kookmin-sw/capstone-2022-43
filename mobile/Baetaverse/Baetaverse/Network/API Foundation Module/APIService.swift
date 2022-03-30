9fb5b9dc22aedef18283aebb3e9777cd64bb3488
JeongTaek Han <smart8612@gmail.com>
2022-03-30 16:26:30 +0900

    feat: APIService 타입 구현 #21

---
+++ mobile/Baetaverse/Baetaverse/Network/API Foundation Module/APIService.swift
@@ -0,0 +1,14 @@
+//
+//  APIService.swift
+//  Baetaverse
+//
+//  Created by JeongTaek Han on 2022/03/30.
+//
+
+import Foundation
+
+protocol APIService {
+    
+    var urlRequest: URLRequest? { get }
+    
+}
--- mobile/Baetaverse/BaetaverseTests/BaetaverseTests.swift
+++ mobile/Baetaverse/BaetaverseTests/BaetaverseTests.swift
@@ -6,31 +6,28 @@
 //
 
 import XCTest
-@testable import Baetaverse
 
-class BaetaverseTests: XCTestCase {
+final class BaetaverseTests: XCTestCase {
+    
+    private var sutNetworkModel: NetworkService!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.sutNetworkModel = NetworkServiceProvider()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sutNetworkModel = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testExample() async throws {
+        // given
+        let url = URL(string: "127.0.0.1:8080")!
+        let urlRequest = URLRequest(url: url)
+        
+        // when
+        let result = try? await sutNetworkModel.fetchData(for: urlRequest)
+        
+        XCTAssertNotNil(result)
     }
 
 }
