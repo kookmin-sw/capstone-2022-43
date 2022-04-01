2b11251daef574e63d38e0b1f3ad87ed93364c26
sboseong <sboseong@kookmin.ac.kr>
2022-04-02 03:44:08 +0900

    Update hscode_request.py

--- HScode/hscode_request.py
+++ HScode/hscode_request.py
@@ -1,10 +1,6 @@
-import time
-starttime = time.time()
-
 import sys
 import hscode_object
 
-hscodetree = hscode_object.hscode_tree()
+hscodetree = hscode_object.hscode_linkedlist()
 
 print(hscodetree.find(sys.argv[1]))
-print("Running Time : ", time.time() - starttime)
