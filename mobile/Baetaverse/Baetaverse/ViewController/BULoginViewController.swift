afda6daab6c3d834b64cb0c9daefdc87866371c1
JeongTaek Han <smart8612@gmail.com>
2022-04-01 20:41:59 +0900

    feat: 로그인 기능 연동 완료 #24

--- mobile/Baetaverse/Baetaverse/ViewController/BULoginViewController.swift
+++ mobile/Baetaverse/Baetaverse/ViewController/BULoginViewController.swift
@@ -8,16 +8,46 @@
 import UIKit
 
 class BULoginViewController: UIViewController {
-
-    @IBOutlet weak var idTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet private weak var idTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var loadActivityIndicatorView: UIActivityIndicatorView!
+    
+    private var appService: AppService? {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return nil
+        }
+        return appDelegate.appService
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+    }
+    @IBAction func loginButtonClicked(_ sender: UIButton) {
+        Task {
+            do {
+                self.loadActivityIndicatorView.startAnimating()
+                try await appService?.login(
+                    email: idTextField.text ?? "",
+                    password: passwordTextField.text ?? ""
+                )
+                self.loadActivityIndicatorView.stopAnimating()
+                self.performSegue(withIdentifier: "presentLoginMainSegue", sender: nil)
+            } catch {
+                self.loadActivityIndicatorView.stopAnimating()
+                let alert = UIAlertController(
+                    title: "로그인에 실패하였습니다!",
+                    message: "계정을 다시 한번 확인해주세요!",
+                    preferredStyle: .alert
+                )
+                alert.addAction(UIAlertAction(title: "확인", style: .default))
+                present(alert, animated: true)
+            }
+        }
     }
     
     @IBAction func unwindToLoginBULoginView(_ segue: UIStoryboardSegue) { }
-
+    
 }
