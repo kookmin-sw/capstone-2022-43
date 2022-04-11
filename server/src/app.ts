7d1991734dfd5217fcb0db87f77ddc65e4f00981
uyimjunho <joonno94@kookmin.ac.kr>
2022-04-12 01:57:07 +0900

    feat: test 환경 구축 jest, supertest 설치 및 test시 app.listen이 실행되지 않도록 함

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,7 @@ import 'dotenv/config';
 import './utils/passport/config';
 import indexRouter from './routes/index';
 import apiRouter from './routes/api';
+import UserInfo from "./middlewares/UserInfo";
 
 
 const app: express.Application = express();
@@ -21,21 +22,22 @@ app.use('/api', apiRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     logger.info(`${req.method} ${req.url}`);
+
     const error =  new HttpException(404, `Not exist ${ req.method } ${ req.url } router`);
     next(error);
 });
 
 app.use((err: HttpException, req: Request, res: Response, next: NextFunction) => {
-    const user = req.headers['user-agent'];
-    const userOS = useragent.parse(user).os.toString();
-    const userBrowser = useragent.parse(user).toAgent();
-    const ip = requestIp.getClientIp(req);
-    const userInfo : string = ` IP: ${ ip } OS : ${ userOS } Browser : ${ userBrowser }`
-
+    const userInfo = new UserInfo(req);
     logger.error(err.status + " " + err.message + userInfo);
     res.status(err.status || 500).send();
 });
 
-app.listen(app.get('port'), '0.0.0.0', () => {
-    logger.info(`Listening http://localhost:${ app.get('port') }`);
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(app.get('port'), '0.0.0.0', () => {
+        logger.info(`Listening http://localhost:${ app.get('port') }`);
+    });
+}
+
+
+module.exports = app;
