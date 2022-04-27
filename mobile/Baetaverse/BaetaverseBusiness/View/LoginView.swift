e919f283d447eea9604ca5a31573cef9c45ed27e
JeongTaek Han <smart8612@gmail.com>
2022-04-27 15:24:18 +0900

    feat: SwiftUI 로그인 뷰 구현 #34

--- mobile/Baetaverse/BaetaverseBusiness/View/LoginView.swift
+++ mobile/Baetaverse/BaetaverseBusiness/View/LoginView.swift
@@ -9,9 +9,94 @@ import SwiftUI
 
 struct LoginView: View {
     
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 25) {
+            LogoView()
+            LoginFormView(
+                username: $username,
+                password: $password
+            )
+            LoginSubmitButton()
+            LoginUserInfoButton()
+        }
+        .padding()
+    }
+    
+}
+
+struct LogoView: View {
+    
+    private let title = "BAETAVERSE"
+    private let subtitle = "LCL 화물 대상 물류비 견적 플랫폼"
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            Text(title)
+                .font(.largeTitle)
+            Text(subtitle)
+                .font(.title2)
+        }
+    }
+    
+}
+
+struct LoginFormView: View {
+    
+    @Binding var username: String
+    @Binding var password: String
+    
+    private let usernameLabel = "아이디"
+    private let passwordLabel = "비밀번호"
+    
+    var body: some View {
+        VStack {
+            TextField(usernameLabel, text: $username)
+            SecureField(passwordLabel, text: $password)
+        }
+        .textFieldStyle(.roundedBorder)
+        .font(.title2)
+    }
+    
+}
+
+struct LoginSubmitButton: View {
+    
+    private let loginLabel = "로그인"
+    
+    var body: some View {
+        Button(action: { }) {
+            HStack {
+                Spacer()
+                Text(loginLabel)
+                Spacer()
+            }
+        }
+        .buttonStyle(.borderedProminent)
+        .controlSize(.large)
+    }
+    
+}
+
+struct LoginUserInfoButton: View {
+    
+    private let signUpLabel = "회원가입"
+    private let findUserInfoLabel = "아이디 / 비밀번호 찾기"
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            Button(action: { }) {
+                Text(signUpLabel)
+            }
+            Spacer()
+            Button(action: { }) {
+                Text(findUserInfoLabel)
+            }
+            Spacer()
+        }
     }
     
 }
