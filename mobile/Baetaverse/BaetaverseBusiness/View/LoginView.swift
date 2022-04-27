05bb7268091b78b7d572b011f9a504d82e54d9c5
JeongTaek Han <smart8612@gmail.com>
2022-04-27 13:46:02 +0900

    file: 디렉토리 구조 리팩토링 #34

--- mobile/Baetaverse/BaetaverseBusiness/View/LoginView.swift
+++ mobile/Baetaverse/BaetaverseBusiness/View/LoginView.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoginView: View {
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoginView()
     }
 }
