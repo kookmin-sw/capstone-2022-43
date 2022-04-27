447c1e880228cb60549c409b82c6915a342e3896
JeongTaek Han <smart8612@gmail.com>
2022-04-27 13:12:33 +0900

    feat: 배타버스 비즈니스 앱 타깃 추가 #34

---
+++ mobile/Baetaverse/BaetaverseBusiness/BaetaverseBusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaetaverseBusinessApp.swift
+//  BaetaverseBusiness
+//
+//  Created by JeongTaek Han on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct BaetaverseBusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
