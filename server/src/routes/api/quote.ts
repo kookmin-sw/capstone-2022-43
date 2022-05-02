239dcdd092810022ae7e8d1f60799ac2773e2d9f
uyimjunho <joonno94@kookmin.ac.kr>
2022-05-02 20:11:26 +0900

    refactor: 견적서 id로 조회 추가

--- server/src/routes/api/quote.ts
+++ server/src/routes/api/quote.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 import requestRouter from "./quote/request";
 import listRouter from './quote/list';
-
+import quotation from "../../middlewares/quoteid";
 
 const router = express.Router();
 const lowRouters = [
@@ -12,5 +12,7 @@ const lowRouters = [
 lowRouters.forEach((lowRouter: Router) => {
     router.use('/quote', lowRouter);
 });
+
+router.get('/quote/:quoteId', quotation);
 
 export default router;
