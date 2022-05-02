d103bd9092eb2fbf31a36fde2388a63de1188518
uyimjunho <joonno94@kookmin.ac.kr>
2022-05-02 20:10:31 +0900

    refactor: 화주, 포워더 회원 도메인 추가

---
+++ server/src/domain/Shipper.ts
@@ -0,0 +1,29 @@
+class Shipper {
+    private _name: String;
+    private _id: String;
+
+    constructor(name: String, id: String) {
+        this._name = name;
+        this._id = id;
+
+    }
+
+    get name(): String {
+        return this._name;
+    }
+
+    set name(value: String) {
+        this._name = value;
+    }
+
+    get id(): String {
+        return this._id;
+    }
+
+    set id(value: String) {
+        this._id = value;
+    }
+
+}
+
+export default Shipper;
