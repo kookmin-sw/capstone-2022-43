1c110a877a727ad2089d78cc7ed59aa277dfb5d6
JeongTaek Han <smart8612@gmail.com>
2022-05-03 13:08:07 +0900

    feat: EstimateCellView 구현 #34

--- mobile/Baetaverse/BaetaverseBusiness/View/Main/RecievedEstimatesView.swift
+++ mobile/Baetaverse/BaetaverseBusiness/View/Main/RecievedEstimatesView.swift
@@ -17,10 +17,40 @@ struct RecievedEstimatesView: View {
     
 }
 
+fileprivate struct EstimateCellView: View {
+    
+    @Binding var cellIndex: Int
+    @Binding var productName: String
+    @Binding var departure: String
+    @Binding var destination: String
+    @Binding var incoterms: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("No. \(String(cellIndex))")
+            Text("물품명 \(productName)")
+            HStack {
+                Text("출발지 \(departure)")
+                Image(systemName: "arrowtriangle.right.fill")
+                Text("도착지 \(destination)")
+            }
+            Text("인코텀즈 \(incoterms)")
+        }
+    }
+    
+}
+
 struct RecievedEstimatesView_Previews: PreviewProvider {
     
     static var previews: some View {
         RecievedEstimatesView()
+        EstimateCellView(
+            cellIndex: .constant(1),
+            productName: .constant("새로운 맥북 프로 14인치"),
+            departure: .constant("대한민국"),
+            destination: .constant("중국"),
+            incoterms: .constant("ABC")
+        )
     }
     
 }
