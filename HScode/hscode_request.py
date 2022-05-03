e5e3302402d3302a1007afce60ff0435bd5e9807
sboseong <sboseong@kookmin.ac.kr>
2022-05-03 16:40:02 +0900

    오류로 인한 수정

--- HScode/hscode_request.py
+++ HScode/hscode_request.py
@@ -6,27 +6,27 @@ from jamo import h2j, j2hcj
 
 starttime = time.time()
 
-input_value = sys.argv[1]
+input_value = sys.argv
 number_cond = re.compile('\d+')
 
 with open('hscode_object.pkl', 'rb') as f:
     hscode = pickle.load(f)
 
 # Blank
-if len(sys.argv) == 1:
+if len(input_value) == 1:
     print([])
 # Input Number
-elif number_cond.search(input_value) != None:
-    print(hscode.code_to_data(input_value))
+elif number_cond.search(input_value[1]) != None:
+    print(hscode.code_to_data(input_value[1]))
 # Input Data
 else:
-    if len(input_value) > 1:
+    if len(input_value[1]) > 1:
         new_input_value = ''
 
-        for unigram in input_value:
+        for unigram in input_value[1]:
             new_input_value += unigram
 
-            if input_value[-1] != unigram:
+            if input_value[1][-1] != unigram:
                 new_input_value += '.'   
 
         input_value = j2hcj(h2j(new_input_value))
