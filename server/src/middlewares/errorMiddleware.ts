e6d2e8a91f9ab650630d66c43843339a2f336cfe
ASak1104 <khw56184@gmail.com>
2022-05-11 02:12:45 +0900

    feat(logger): add status code to error log
    issue #36

--- server/src/middlewares/errorMiddleware.ts
+++ server/src/middlewares/errorMiddleware.ts
@@ -1,15 +1,13 @@
 import HttpException from "../exceptions/HttpException";
 import { NextFunction, Request, Response } from "express";
-import useragent from "useragent";
-import requestIp from "request-ip";
 import logger from "../utils/logger";
 import userAgent from "../@types/UserAgent";
 
 
 const errorMiddleware = (err: HttpException, req: Request, res: Response, next: NextFunction): void => {
     const userInfo = new userAgent(req);
-    logger.error(err.status + " " + err.message + userInfo);
     const status = err.status || 500;
+    logger.error(`${ status } ${ err.message } ${ decodeURI(req.originalUrl) } ${ userInfo }`);
     res.status(status).send({ status, message: err.message });
 };
 
