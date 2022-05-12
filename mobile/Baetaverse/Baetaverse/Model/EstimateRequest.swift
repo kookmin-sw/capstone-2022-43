c9ab8e0dbfab31e3941fc9b8784db44f56b55773
JeongTaek Han <smart8612@gmail.com>
2022-05-12 23:36:48 +0900

    feat: EstimateRequest 엔티티 구현 #44

---
+++ mobile/Baetaverse/Baetaverse/Model/EstimateRequest.swift
@@ -0,0 +1,22 @@
+//
+//  EstimateRequest.swift
+//  Baetaverse
+//
+//  Created by JeongTaek Han on 2022/05/12.
+//
+
+import Foundation
+
+struct EstimateRequest {
+    
+    let tradeType: String
+    let tradeDetail: String
+    let forwardingDate: Int
+    let departureCountry: String
+    let departureDetail: String
+    let destinationCountry: String
+    let destinationDetail: String
+    let incoterms: String
+    let closingDate: Date
+    
+}
