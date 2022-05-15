3884f3a8b986f37448389c126cf3fd015352a0f0
ASak1104 <khw56184@gmail.com>
2022-05-16 04:07:49 +0900

    feat(db): set environment for typeorm and mysql2 package
    issue #36

--- server/src/@types/node/environment.d.ts
+++ server/src/@types/node/environment.d.ts
@@ -1,3 +1,6 @@
+import { num, port, str, url } from "envalid";
+
+
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
@@ -8,6 +11,12 @@ declare global {
             JWT_OWNER_SECRET: string,
             JWT_FORWARDER_SECRET: string,
             HSCODE_SERVER_HOST: string,
+            MYSQL_SERVER_HOST: string,
+            MYSQL_SERVER_USER: string,
+            MYSQL_SERVER_PASSWORD: string,
+            MYSQL_SERVER_PORT: number,
+            MYSQL_SERVER_DATABASE: string,
+            MYSQL_SERVER_CONNECTION_LIMIT: number,
         }
     }
 }
