086ea52e561fa75ff1f24bd887e9f1c6bdd93299
JeongTaek Han <smart8612@gmail.com>
2022-05-16 21:40:25 +0900

    feat: 백업 #44

---
+++ mobile/Baetaverse/Baetaverse/Network/APIModel/ResponseModel/QuotationsResponse.swift
@@ -0,0 +1,30 @@
+//
+//  QuotationsResponse.swift
+//  Baetaverse
+//
+//  Created by JeongTaek Han on 2022/05/16.
+//
+
+import Foundation
+
+extension APIResponseModel {
+    
+    struct QuotationsResponse: Decodable {
+        
+        let status: Int
+        let message: String
+        let quotation: APIEntity.Quotation
+        let selectedGoods: [APIEntity.GoodsDetail]
+        
+        enum CodingKeys: String, CodingKey {
+            
+            case status
+            case message
+            case quotation = "Quotation"
+            case selectedGoods
+            
+        }
+        
+    }
+    
+}
