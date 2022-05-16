4b22949eb7e744c4e108332e732d255598b616ab
JeongTaek Han <smart8612@gmail.com>
2022-05-16 20:35:16 +0900

    feat: RegisterEstimateRequest 모델 구현 #44

---
+++ mobile/Baetaverse/Baetaverse/Model/Estimate.swift
@@ -0,0 +1,18 @@
+//
+//  Estimate.swift
+//  Baetaverse
+//
+//  Created by JeongTaek Han on 2022/05/16.
+//
+
+import Foundation
+
+struct Estimate {
+    
+    var requestId: Int
+    var oceanRightPrice: Int
+    var inlandFreightPrice: Int
+    var totalPrice: Int
+    var estimatedTime: Int
+    
+}
