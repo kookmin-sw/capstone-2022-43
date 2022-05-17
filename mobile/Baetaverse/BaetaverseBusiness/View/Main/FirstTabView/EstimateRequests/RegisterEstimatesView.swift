137b80d7998e69351f524ca6fcd5e80027e8402d
JeongTaek Han <smart8612@gmail.com>
2022-05-17 21:21:53 +0900

    feat: 견적 등록 기능 구현 #45

--- mobile/Baetaverse/BaetaverseBusiness/View/Main/FirstTabView/EstimateRequests/RegisterEstimatesView.swift
+++ mobile/Baetaverse/BaetaverseBusiness/View/Main/FirstTabView/EstimateRequests/RegisterEstimatesView.swift
@@ -9,33 +9,55 @@ import SwiftUI
 
 struct RegisterEstimatesView: View {
     
-    @Environment(\.dismiss) private var dismiss
     let id: Int
     
+    @Environment(\.dismiss) private var dismiss
     @StateObject private var viewModel = RegisterEstimatesViewModel()
+    @State private var oceanFreightPrice = ""
+    @State private var inlandFreightPrice = ""
+    @State private var totalPrice = ""
+    @State private var estimatedTime = ""
+    
+    private var estimate: Estimate {
+        Estimate(
+            requestId: id,
+            oceanFreightPrice: Int(oceanFreightPrice) ?? 0,
+            inlandFreightPrice: Int(inlandFreightPrice) ?? 0,
+            totalPrice: Int(totalPrice) ?? 0,
+            estimatedTime: Int(estimatedTime) ?? 0
+        )
+    }
     
     var body: some View {
         NavigationView {
-            RegisterEstimatesContentView()
-                .navigationTitle("견적서 등록하기")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Button(role: .cancel, action: { dismiss() }) {
-                            Text("닫기")
-                        }
+            RegisterEstimatesContentView(
+                oceanFreightPrice: $oceanFreightPrice,
+                inlandFreightPrice: $inlandFreightPrice,
+                totalPrice: $totalPrice,
+                estimatedTime: $estimatedTime
+            )
+            .navigationTitle("견적서 등록하기")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(role: .cancel, action: { dismiss() }) {
+                        Text("닫기")
                     }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(role: .none, action: {
-                            Task {
-                                try? await viewModel.register(estimate:)
-                            }
-                        }) {
-                            Text("등록")
-                        }
-                    }
-                    
                 }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(role: .none, action: registerButtonClicked) {
+                        Text("등록")
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    private func registerButtonClicked() {
+        Task {
+            try? await viewModel.register(estimate: estimate)
+            dismiss()
         }
     }
     
@@ -43,13 +65,18 @@ struct RegisterEstimatesView: View {
 
 private struct RegisterEstimatesContentView: View {
     
+    @Binding var oceanFreightPrice: String
+    @Binding var inlandFreightPrice: String
+    @Binding var totalPrice: String
+    @Binding var estimatedTime: String
+    
     var body: some View {
         Form {
             List {
-                TextField("해상운송비용", text: .constant(""))
-                TextField("육상운송비용", text: .constant(""))
-                TextField("예상운송시간", text: .constant(""))
-                TextField("총 운송비용", text: .constant(""))
+                TextField("해상운송비용", text: $oceanFreightPrice)
+                TextField("육상운송비용", text: $inlandFreightPrice)
+                TextField("예상운송시간", text: $estimatedTime)
+                TextField("총 운송비용", text: $totalPrice)
             }
         }
     }
