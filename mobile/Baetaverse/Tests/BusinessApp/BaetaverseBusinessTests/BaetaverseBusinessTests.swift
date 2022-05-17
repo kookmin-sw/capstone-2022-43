253166226cedb36f1f8788a0d92417e2557c3026
JeongTaek Han <smart8612@gmail.com>
2022-05-17 11:55:34 +0900

    feat: 배타버스 API 공통 모듈화 #45

--- mobile/Baetaverse/Tests/BusinessApp/BaetaverseBusinessTests/BaetaverseBusinessTests.swift
+++ mobile/Baetaverse/Tests/BusinessApp/BaetaverseBusinessTests/BaetaverseBusinessTests.swift
@@ -10,26 +10,267 @@ import XCTest
 
 class BaetaverseBusinessTests: XCTestCase {
 
+    private var sutBaetaverse: AppService!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.sutBaetaverse = BaetaverseAppService.configure()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.sutBaetaverse = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_알맞은_계정이_입력되면_정상적으로_로그인되어야한다() async throws {
+        // given
+        let email = "test8@test8.com"
+        let password = "12341234"
+        
+        // when
+        try await sutBaetaverse.login(
+            permission: .owner,
+            email: email,
+            password: password
+        )
+        
+        // then
+        XCTAssertTrue(sutBaetaverse.isLogin)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_비밀번호가_잘못된_계정이_입력되면_오류를_반환해야한다() async throws {
+        // given
+        let email = "test1@test1.com"
+        let password = "1234"
+        
+        // when then
+        do {
+            try await sutBaetaverse.login(
+                permission: .owner,
+                email: email,
+                password: password
+            )
+            XCTFail("로그인에 실패해야 하는데 성공하는 오류 발생")
+        } catch {
+            XCTAssertTrue(true)
+        }
+    }
+    
+    func test_없는_사용자_계정으로_로그인하면_오류를_반환해야한다() async throws {
+        // given
+        let email = "unknown@unknown.com"
+        let password = "1234"
+        
+        // when then
+        do {
+            try await sutBaetaverse.login(
+                permission: .forwarder,
+                email: email,
+                password: password
+            )
+            XCTFail("로그인에 실패해야 하는데 성공하는 오류 발생")
+        } catch {
+            XCTAssertTrue(true)
+        }
+    }
+    
+    func test_회원가입_정상적으로_동작해야한다() async throws {
+        // given
+        let email = "test8@test8.com"
+        let password = "12341234"
+        let name = "testUserName8"
+        let phoneNumber = "010-1234-5678"
+        
+        // when then
+        do {
+            try await sutBaetaverse.signUp(
+                permission: .forwarder,
+                email: email,
+                password: password,
+                name: name,
+                phoneNumber: phoneNumber,
+                corporationName: "훌륭한 회사",
+                corporationNumber: "070-1234-5678"
+            )
+        } catch {
+            XCTFail("\(error.localizedDescription)")
+        }
+    }
+    
+    func test_이미_등록된_사용자가_회원가입하면_오류를_반환해야한다() async throws {
+        // given
+        let email = "test1@test1.com"
+        let password = "12341234"
+        let name = "testUserName"
+        let phoneNumber = "010-1234-5678"
+        
+        // when then
+        do {
+            try await sutBaetaverse.signUp(
+                permission: .forwarder,
+                email: email,
+                password: password,
+                name: name,
+                phoneNumber: phoneNumber,
+                corporationName: "훌륭한 회사",
+                corporationNumber: "070-1234-5678"
+            )
+            XCTFail("사용자가 중복 가입되었습니다")
+        } catch {
+            XCTAssertTrue(true)
+        }
+    }
+    
+    func test_010121_코드가_입력되면_살아있는_말이_반환되어야_한다() async throws {
+        // given
+        let email = "test1@test1.com"
+        let password = "12341234"
+        let code = "010121"
+        
+        // when
+        try await sutBaetaverse.login(
+            permission: .owner,
+            email: email,
+            password: password
+        )
+        
+        let result = try await sutBaetaverse.queryHSCode(code: code)
+        
+        // then
+        XCTAssertEqual(result, ["010121 : 말(번식용,산동물)"])
+    }
+    
+    func test_로그인된_사용자의_견적요청서_명단이_조회되어야한다() async throws {
+        // given
+        let email = "test1@test1.com"
+        let password = "12341234"
+        
+        try await sutBaetaverse.login(
+            permission: .owner,
+            email: email,
+            password: password
+        )
+        
+        // when
+        let result = try? await sutBaetaverse.queryEstimateRequests()
+        if let result = result {
+            print(result)
+        }
+        
+        // then
+        XCTAssertNotNil(result)
+    }
+    
+    func test_새로운_견적_등록하기가_정상적으로_동작해야한다() async throws {
+        // given
+        let email = "test1@test1.com"
+        let password = "12341234"
+        
+        let estimateRequest = EstimateRequest(
+            id: nil,
+            tradeType: "수입",
+            tradeDetail: "Port to Port",
+            forwardingDate: Date(),
+            departureCountry: "USA",
+            departureDetail: "NewYork",
+            destinationCountry: "South Korea",
+            destinationDetail: "Seoul",
+            incoterms: "DEF",
+            closingDate: Date(),
+            createdAt: nil
+        )
+        
+        let products = [
+            Product(
+                id: nil,
+                name: "Apple MacBook Pro",
+                price: 2490000,
+                weight: 10,
+                standardUnit: "Kg",
+                hsCode: "123456",
+                createdAt: nil
+            )
+        ]
+        
+        try await sutBaetaverse.login(
+            permission: .owner,
+            email: email,
+            password: password
+        )
+        
+        // when
+        try await sutBaetaverse.registerEvaluateRequest(
+            estimateRequest: estimateRequest,
+            products: products
+        )
+        
+        // then
+        XCTAssert(true)
+    }
+    
+    func test_로그인한_사용자의_2번_견적서가_정상적으로_조회되어야_한다() async throws {
+        // given
+        let email = "test1@test1.com"
+        let password = "12341234"
+        
+        try await sutBaetaverse.login(
+            permission: .owner,
+            email: email,
+            password: password
+        )
+        
+        // when then
+        do {
+            _ = try await sutBaetaverse.queryEstimateRequestDetail(id: "2")
+            XCTAssert(true)
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+    
+    func test_2번_견적요청서에_견적이_정상적으로_작성되어야한다() async throws {
+        // given
+        let email = "test8@test8.com"
+        let password = "12341234"
+        let estimate = Estimate(
+            requestId: 2,
+            oceanFreightPrice: 100,
+            inlandFreightPrice: 123,
+            totalPrice: 223,
+            estimatedTime: 5
+        )
+        
+        try await sutBaetaverse.login(
+            permission: .forwarder,
+            email: email,
+            password: password
+        )
+        
+        // when then
+        do {
+            try await sutBaetaverse.registerEstimate(estimate: estimate)
+            XCTAssert(true)
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+    
+    func test_로그인한_사용자의_견적서_명단을_모두_반환해야한다() async throws {
+        // given
+        let email = "test8@test8.com"
+        let password = "12341234"
+        
+        try await sutBaetaverse.login(
+            permission: .forwarder,
+            email: email,
+            password: password
+        )
+        
+        // when then
+        do {
+            let result = try await sutBaetaverse.queryEstimates()
+            print(result)
+            XCTAssert(true)
+        } catch {
+            XCTFail("\(error)")
         }
     }
 
