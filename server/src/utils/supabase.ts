62940ad6f8a2e88f41adcb23c20f626b1c862f3c
ASak1104 <khw56184@gmail.com>
2022-05-19 21:19:00 +0900

    remove: remove unused files
    issue #36

--- server/src/utils/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-
-const supabaseUrl: string = process.env.PUBLIC_SUPABASE_URL || '';
-const supabaseServerKey: string = process.env.PUBLIC_SUPABASE_KEY || '';
-
-const supabase: SupabaseClient = createClient(supabaseUrl, supabaseServerKey);
-
-export { supabase };
