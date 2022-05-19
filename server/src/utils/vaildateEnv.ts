4cc2efc8687c0f688aa250b40bc2dccd465b527e
ASak1104 <khw56184@gmail.com>
2022-05-19 21:19:57 +0900

    edit: remove supabase data
    issue #36

--- server/src/utils/vaildateEnv.ts
+++ server/src/utils/vaildateEnv.ts
@@ -3,8 +3,6 @@ import { cleanEnv, str, port, url } from 'envalid';
 
 const validateForwarderEnv = () => {
     cleanEnv(process.env, {
-        PUBLIC_SUPABASE_URL: str(),
-        PUBLIC_SUPABASE_KEY: str(),
         JWT_FORWARDER_SECRET: str(),
         PORT: port(),
         HSCODE_SERVER_HOST: url(),
