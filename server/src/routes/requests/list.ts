6a9a55c12ac1c1455275c0897777cb4b4189fc4e
ASak1104 <khw56184@gmail.com>
2022-05-20 15:17:39 +0900

    fix: add id column in requests/list
    issue #67

--- server/src/routes/requests/list.ts
+++ server/src/routes/requests/list.ts
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/list', verifyForwarderToken, async (req : Request, res : Response, next : NextFunction) => {
     try {
         const list = await requestRepository.find({
-            select: ['trade_type', 'trade_detail', 'forwarding_date', 'departure_country', 'departure_detail',
+            select: ['id', 'trade_type', 'trade_detail', 'forwarding_date', 'departure_country', 'departure_detail',
                 'destination_country', 'destination_detail', 'incoterms', 'closing_date', 'created_at']
         });
 
